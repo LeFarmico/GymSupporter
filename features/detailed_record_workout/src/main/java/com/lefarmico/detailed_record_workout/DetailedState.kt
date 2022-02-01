d4af254e2c621c465c41f3a905fc1211ad3a2135
Artyom Zharnicovich <62179029+LeFarmico@users.noreply.github.com>
2022-02-01 19:22:14 +0100

    remove logic from DetailedWorkoutRecordFragment.kt

--- features/detailed_record_workout/src/main/java/com/lefarmico/detailed_record_workout/DetailedState.kt
+++ features/detailed_record_workout/src/main/java/com/lefarmico/detailed_record_workout/DetailedState.kt
@@ -6,7 +6,8 @@ import java.lang.Exception
 
 sealed class DetailedState : BaseState.State {
     data class WorkoutResult(
-        val workout: WorkoutRecordsViewData.WorkoutWithExercisesAndSets
+        val workout: WorkoutRecordsViewData.Workout,
+        val exercises: List<WorkoutRecordsViewData.ViewDataItemType>
     ) : DetailedState()
     data class DateResult(val dateText: String) : DetailedState()
     data class TitleResult(val title: String) : DetailedState()
--- features/detailed_record_workout/src/main/java/com/lefarmico/detailed_record_workout/StateHandler.kt
+++ features/detailed_record_workout/src/main/java/com/lefarmico/detailed_record_workout/StateHandler.kt
@@ -1,6 +1,7 @@
 package com.lefarmico.detailed_record_workout
 
 import com.lefarmico.core.base.BaseState
+import com.lefarmico.core.entity.WorkoutRecordsViewData
 import com.lefarmico.core.mapper.toViewData
 import com.lefarmico.domain.entity.WorkoutRecordsDto
 import com.lefarmico.domain.utils.DataState
@@ -13,6 +14,17 @@ fun DataState<WorkoutRecordsDto.WorkoutWithExercisesAndSets>.reduce(
     return when (this) {
         is DataState.Error -> DetailedEvent.ExceptionResult(exception)
         DataState.Loading -> DetailedEvent.Loading
-        is DataState.Success -> DetailedState.WorkoutResult(data.toViewData(dateFormatter, timeFormatter))
+        is DataState.Success -> {
+            val dto = this.data
+            val workout = dto.workout.toViewData(dateFormatter, timeFormatter)
+            val exercises = mutableListOf<WorkoutRecordsViewData.ViewDataItemType>()
+            // TODO : Избавиться от логики
+            val exerciseListViewData = dto.exerciseWithSetsList.toViewData()
+            for (i in exerciseListViewData.indices) {
+                exercises.add(exerciseListViewData[i].exercise)
+                exercises.addAll(exerciseListViewData[i].setList)
+            }
+            DetailedState.WorkoutResult(workout, exercises)
+        }
     }
 }
